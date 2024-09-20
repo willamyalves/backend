6990625a9b7bc38f0a94c85d77bab6ea9e907b59
Willamy Alves Gomes <willamy9100@gmail.com>
2024-09-20 08:43:50 -0300

    .

---
+++ GET_A_PET_Node.js/frontend/src/components/pages/layout/Navbar.tsx
@@ -0,0 +1,17 @@
+// Router
+import { Link } from "react-router-dom";
+
+// Logo
+import Logo from "../ ../assets/react.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src={Logo} alt="Get a Pet" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

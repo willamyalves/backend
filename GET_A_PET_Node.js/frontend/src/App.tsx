9674f9c900a3dbdc3a96adad9cb08d685dca151b
Willamy Alves Gomes <willamy9100@gmail.com>
2024-09-20 00:01:56 -0300

    .

--- GET_A_PET_Node.js/frontend/src/App.tsx
+++ GET_A_PET_Node.js/frontend/src/App.tsx
@@ -1,9 +1,14 @@
+// router
+import { Outlet } from "react-router-dom";
+
+// CSS
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Get a pet</h1>
+      <Outlet />
     </div>
   );
 }
--- GET_A_PET_Node.js/frontend/src/main.tsx
+++ GET_A_PET_Node.js/frontend/src/main.tsx
@@ -1,10 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Components
+import Home from "./components/pages/Home.tsx";
+import Login from "./components/pages/auth/Login.tsx";
+import Register from "./components/pages/auth/Register.tsx";
+
+// Router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
